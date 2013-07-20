1bef1b9c624f856ceb43e5c0539d583db3ec2b6b
fla4112 <melfuad@yahoo.com>
2013-07-20 15:37:31 +0300

    Create hw0.py

---
+++ hw0.py
@@ -0,0 +1,59 @@
+# Please fill out this stencil and submit using the provided submission script.
+
+
+
+
+
+## Problem 1
+def my_filter(L, num): return [x for x in L if x%num != 0]
+
+
+
+## Problem 2
+def myLists(L): return [ L[0:x+1] for x in range(len(L)) if (L[x]>=0)]
+
+
+
+## Problem 3
+def myFunctionComposition(f, g): return { k:g[v] for (k,v) in f.items() }
+
+
+## Problem 4
+# Please only enter your numerical solution.
+
+complex_addition_a = (5+3j)
+complex_addition_b = (1j)
+complex_addition_c = (-1 + .001j)
+complex_addition_d = (.001 + 9j)
+
+
+
+## Problem 5
+GF2_sum_1 = 1
+GF2_sum_2 = 0
+GF2_sum_3 = 0
+
+
+## Problem 6
+def mySum(L): pass
+
+
+
+## Problem 7
+def myProduct(L): pass
+
+
+
+## Problem 8
+def myMin(L): pass
+
+
+
+## Problem 9
+def myConcat(L): pass
+
+
+
+## Problem 10
+def myUnion(L): pass
+
