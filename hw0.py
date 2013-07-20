d232215cb8261bb7fa44444b16a04460ad0540ed
fla4112 <melfuad@yahoo.com>
2013-07-20 20:21:50 +0300

    Update hw0.py

--- hw0.py
+++ hw0.py
@@ -1,22 +1,12 @@
 # Please fill out this stencil and submit using the provided submission script.
 
 
-
-
-
 ## Problem 1
 def my_filter(L, num): return [x for x in L if x%num != 0]
-
-
-
 ## Problem 2
 def myLists(L): return [ L[0:x+1] for x in range(len(L)) if (L[x]>=0)]
-
-
-
 ## Problem 3
 def myFunctionComposition(f, g): return { k:g[v] for (k,v) in f.items() }
-
 
 ## Problem 4
 # Please only enter your numerical solution.
@@ -26,34 +16,45 @@ complex_addition_b = (1j)
 complex_addition_c = (-1 + .001j)
 complex_addition_d = (.001 + 9j)
 
-
-
 ## Problem 5
 GF2_sum_1 = 1
 GF2_sum_2 = 0
 GF2_sum_3 = 0
-
-
 ## Problem 6
-def mySum(L): pass
-
-
+def mySum(L): 
+    current= 0
+    for x in range(len(L)):
+        current= current + L[x]
+    return current 
 
 ## Problem 7
-def myProduct(L): pass
+def myProduct(L):
+    current= 1
+    for x in range(len(L)):
+        current= current * L[x]
+    return current
 
 
 
 ## Problem 8
-def myMin(L): pass
-
+def myMin(L):
+    current= L[0]
+    for x in range(1, len(L)):
+        if(L[x] < current):
+            current= L[x]
+    return current
 
 
 ## Problem 9
-def myConcat(L): pass
-
-
+def myConcat(L):
+    current= L[0]
+    for x in range(1, len(L)):
+        current += L[x]
+    return current
 
 ## Problem 10
-def myUnion(L): pass
-
+def myUnion(L):
+    current= L[0]
+    for x in range(1, len(L)):
+        current= current | L[x]
+    return current
