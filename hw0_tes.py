b804f4179e4a0691f7904e73cb120b993d3d7624
fla4112 <melfuad@yahoo.com>
2013-07-20 20:20:51 +0300

    Update hw0_tes.py

--- hw0_tes.py
+++ hw0_tes.py
@@ -1,5 +1,8 @@
+from GF2 import one, zero
+print(one*one+zero)
+
 # 1
-L= [1,2,4,5,7,0]
+L= [1,2,4,5,7]
 num= 2
 def my_filter(L, num): return [x for x in L if x%num != 0]
 print(my_filter(L, num))
@@ -21,7 +24,44 @@ g = {'a':'apple', 'b':'banana'}
 
 print({ k:g[v] for (k,v) in f.items() })
 
-from GF2 import one, zero
 print(one+one+one+ zero)
 print(one*one+ zero*one+zero*zero+one*one)
 print((one+one+one)*(one+one+one+one))
+
+def mySum(L): 
+    current= 0
+    for x in range(len(L)):
+        current= current + L[x]
+    return current   
+print(mySum(L ))
+
+def myProduct(L):
+    current= 1
+    for x in range(len(L)):
+        current= current * L[x]
+    return current
+print(myProduct(L))
+
+def myMin(L):
+    current= L[0]
+    for x in range(1, len(L)):
+        if(L[x] < current):
+            current= L[x]
+    return current
+print (myMin(L))
+
+words=['hello', 'world', 'hi']
+def myConcat(L):
+    current= L[0]
+    for x in range(1, len(L)):
+        current += L[x]
+    return current
+print(myConcat(words))
+
+def myUnion(L):
+    current= L[0]
+    for x in range(1, len(L)):
+        current= current | L[x]
+    return current
+st = [ set([0, 1]), set([1, 2]) , set([2, 3]), set([10, 'f']) ]
+print(myUnion(st))
